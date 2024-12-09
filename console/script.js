9d5ea2b7cef58d05a644723d965b3d090bc8c32d
Yura <kyy101000@gmail.com>
2024-12-09 14:15:58 -0600

    Some basic UI added

--- console/script.js
+++ console/script.js
@@ -22,56 +22,56 @@ function getHumanChoice() {
     } else {
         console.log('Invalid input')
         return;
-    } 
+    }
 }
 
+let computerScore = 0;
+let humanScore = 0;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === null || humanChoice === undefined) {
-            return;
-        }
+const result = document.querySelector("#result");
+result.textContent = `Computer: ${computerScore}, Human: ${humanScore}`
 
-
-        if (humanChoice === computerChoice) {
-            console.log(`You both chose ${humanChoice}`);
-            return;
-        }
-    
-        if (computerChoice === 'rock' && humanChoice === 'scissors') {
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
-            computerScore+=1;
-        } else if (computerChoice === 'paper' && humanChoice === 'rock') {
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
-            computerScore+=1;
-        } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
-            console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
-            computerScore+=1;
-        } else {
-            console.log(`You won! ${humanChoice} beats ${computerChoice}`);
-            humanScore+=1;
-        }
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        const notification = document.querySelector("#notification");
+        notification.textContent = 'You chose the same!'
+        return;
     }
 
-    for (i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-
-    if (humanScore === computerScore) {
-        console.log('Friendship won')
-    } else if (humanScore > computerScore) {
-        console.log('Human beats machine!')
+    notification.textContent = '';
+    if (computerChoice === 'rock' && humanChoice === 'scissors') {
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+    } else if (computerChoice === 'paper' && humanChoice === 'rock') {
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+    } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
     } else {
-        console.log('Computer beats fleshbag!')
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
     }
 
-    console.log('New game!')
-    humanScore = 0;
-    computerScore = 0;
+
 }
 
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        playRound(e.target.id, getComputerChoice());
+        if (computerScore === 5) {
+            result.textContent = `Computer won`
+            computerScore = 0;
+            humanScore = 0;
+        } else if (humanScore === 5) {
+            result.textContent = `Human won`
+            computerScore = 0;
+            humanScore = 0;
+        } else {
+            result.textContent = `Computer: ${computerScore}, Human: ${humanScore}`
+        }
+    })
+})
